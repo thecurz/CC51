71ab0ec8c31414181e2aabd13e3fd132a9284e4c
CurZ <carloscastillaochoa@gmail.com>
2023-03-28 23:22:52 -0500

    Removed artifacts from Samuel's Week #1 code

--- Semana 01/Hoja 01/Samuel Cano/dos.cpp
+++ Semana 01/Hoja 01/Samuel Cano/dos.cpp
@@ -1,21 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#include <bits/stdc++.h>
+using namespace std;
 
-\f0\fs24 \cf0 #include <bits/stdc++.h>\
-using namespace std;\
-\
-int main() \{\
-	string s;\
-	cin>>s;\
-	string a,b,c;\
-	a=s.substr(0,2);\
-	b=s.substr(2,1);\
-	c=s.substr(3,2);\
-	cout<<stoi(a)+stoi(b)+stoi(c);\
-	return 0;\
-\}\
+int main() {
+    string s;
+    cin >> s;
+    string a, b, c;
+    a = s.substr(0, 2);
+    b = s.substr(2, 1);
+    c = s.substr(3, 2);
+    cout << stoi(a) + stoi(b) + stoi(c);
+    return 0;
 }
